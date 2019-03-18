64f76f1be22c12b0e17bd68e5e41aa98aeab218c
Mymy69 <47871293+Mymy69@users.noreply.github.com>
2019-03-18 16:07:27 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,71 @@
+<?php
+include("header.html")
+
+?>
+
+
+<body>
+
+
+    <!-- CONTENT START -->
+    <footer class="">
+        <div class=" container-fluid fondcontact">
+            
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+
+                        <h1 class="mt-5 text-white titre-centre">CONTACT</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6 ">
+                        <form>
+                            <div class="form-group ">
+                                <label class="text-white" for="exampleFormControlInput1">PSEUDO</label>
+                                <input type="email" class="form-control text-white" id="exampleFormControlInput1" style="background-color:transparent; border-color: white; border-width: medium; border-style: solid">
+                            </div>
+                            <div class="form-group">
+                                <label class="text-white" for="exampleFormControlInput1"> EMAIL</label>
+                                <input type="email" class=" form-control text-white" id="exampleFormControlInput1"
+                                    style="background-color:transparent; border-color: white; border-width: medium; border-style: solid">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-6 p-4">
+                        <p class="text-white text-contact">10, Jabba street<br>
+                            Mos Espa Tatouine<br>
+                            <p class="text-white text-contact">Dark.site@coteobscur.com</p>
+
+                        </p>
+
+                    </div>
+                    <div class="col-12  ">
+                        <div class="form-group ">
+                            <label class="text-white" for="exampleFormControlTextarea1">MESSAGE</label>
+                            <textarea class=" form-control text-white " id="exampleFormControlTextarea1" rows="7" style="background-color:transparent; border-color: white; border-width: medium; border-style: solid"></textarea>
+                            <button type="button" class="btn btn mt-5" style="background-color:#403f3e; color:white; width: 300px">MESSAGE</button>
+                        </div>
+                    </div>
+
+                    <div class="col-12 d-flex justify-content-between">
+                        <a href="index.php"> <img src="img/senat.png"></a>
+                        <a href="padme.php"> <img src="img/padme.png"></a>
+                        <a href="anakin.php"> <img src="img/anakin.png"></a>
+
+                        <a href="leia_luke.php"> <img src="img/luke-leia.png"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+
+    <?php
+include("footer.html")
+
+?>
